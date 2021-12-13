01e125d95f4805b16cad7e206b7da7c01c27abb3
Renaud <cenarius63@hotmail.fr>
2021-12-13 11:03:43 +0100

    first commit

---
+++ src/Entity/Owner.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\OwnerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OwnerRepository::class)]
+#[ApiResource]
+class Owner
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $firstName;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $lastName;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $birthAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getBirthAt(): ?\DateTimeInterface
+    {
+        return $this->birthAt;
+    }
+
+    public function setBirthAt(?\DateTimeInterface $birthAt): self
+    {
+        $this->birthAt = $birthAt;
+
+        return $this;
+    }
+}
