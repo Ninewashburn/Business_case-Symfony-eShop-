96c73cbbefa6868b7e879e8c5b159218e9c7abb6
Ninewashburn <cenarius63@hotmail.fr>
2022-03-26 00:45:44 +0100

    new migration for laptop

--- migrations/Version20220325234228.php
+++ migrations/Version20220325234228.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220321102759 extends AbstractMigration
+final class Version20220325234228 extends AbstractMigration
 {
     public function getDescription(): string
     {
