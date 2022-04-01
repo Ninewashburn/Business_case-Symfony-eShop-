2ebed6d14a771caf0048ead4358499892e6605d9
Washburn <renaud.meynadier@be-ys-software.com>
2022-04-01 16:56:47 +0200

    massive upgrade, store, contact, user log

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -2,19 +2,18 @@
 
 namespace App\Form;
 
-use App\Entity\Address;
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserFormType extends AbstractType
 {
@@ -22,53 +21,70 @@ class UserFormType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, [
-                'label' => false,
+                'label' => 'Email',
                 'attr' => [
                     'placeholder' => 'user.index.table.email',
                 ]
             ])
-            ->add('password', PasswordType::class, [
-                'label' => false,
-                'attr' => [
-                    'placeholder' => 'user.index.table.password',
-                ]
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'label' => 'Mot de passe',
+                'attr' => ['autocomplete' => 'new-password',
+                'placeholder' => 'user.index.table.password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner votre mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
             ])
             ->add('firstName', TextType::class, [
-                'label' => false,
+                'label' => 'Prénom',
                 'attr' => [
                     'placeholder' => 'user.index.table.firstName',
                 ]
             ])
             ->add('lastName', TextType::class, [
-                'label' => false,
+                'label' => 'Nom',
                 'attr' => [
                     'placeholder' => 'user.index.table.lastName',
                 ]
             ])
-            ->add('telephone', NumberType::class, [
-                'label' => false,
-                'attr' => [
-                    'placeholder' => 'user.index.table.telephone',
-                ]
-            ])
             ->add('birth_at', BirthdayType::class, [
-                'label' => false,
+                'label' => 'Date de naissance',
                 'attr' => [
                     'placeholder' => 'user.index.table.telephone',
                 ]
             ])
-            ->add('address', EntityType::class, [
-                'required' => false,
-                'label' => false,
-                'class' => Address::class,
-                'multiple' => true,
-                'choice_label' => function (Address $address) {
-                    return $address->getFirstAddress() . ' ' . $address->getSecondAddress() . ' ' . $address->getCity()
-                        . ' ' . $address->getCountry() . ' ' . $address->getZipCode();
-//                    ->orderBy('addr', 'ASC');
-                },
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
                 'attr' => [
-                    'placeholder' => 'user.index.table.adresse',
+                    'placeholder' => 'user.index.table.address',
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'user.index.table.address',
+                ]
+            ])
+            ->add('country', TextType::class, [
+                'label' => 'Pays',
+                'attr' => [
+                    'placeholder' => 'user.index.table.country',
+                ]
+            ])
+            ->add('telephone', NumberType::class, [
+                'label' => 'Téléphone',
+                'attr' => [
+                    'placeholder' => 'user.index.table.telephone',
                 ]
             ])
             ->add('submit', SubmitType::class, [
